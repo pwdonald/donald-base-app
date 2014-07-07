b7e3b0f282440047488c27b2da3ee81d3c5b62e9
Donald Jones <jones.donald@gmail.com>
2014-07-06 19:10:36 -0500

    Fixed Bower task in Gruntfile. Removed .bowerrc since files are now handled in the grunt task. Also removed bootflat dependency.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,22 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    bowercopy: {
+      options: {
+        clean: true,
+      },
+      libs: {
+        options: {
+          destPrefix: 'public/lib/'
+        },
+        files : {
+          'bootstrap/css': 'bootstrap/dist/css',
+          'bootstrap/js': 'bootstrap/dist/js',
+          'bootstrap/fonts': 'bootstrap/dist/fonts',
+          'jquery': 'jquery/dist'
+        }
+      }
+    },
     less: {
       development: {
         expand: true,
@@ -35,17 +51,19 @@ module.exports = function(grunt) {
   //grunt.loadNpmTasks('grunt-contrib-uglify');
   //grunt.loadNpmTasks('grunt-contrib-jshint');
   //grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   //grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-bowercopy');
 
   //grunt.registerTask('test', ['jshint', 'qunit']);
 
   //grunt.registerTask('default', ['jshint','concat', 'uglify', 'nodemon']);
 
-  grunt.registerTask('default',['less','concurrent:target']);
+  grunt.registerTask('default',['bowercopy:libs', 'less','concurrent:target']);
 
 };
 
