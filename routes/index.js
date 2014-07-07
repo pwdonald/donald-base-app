80d1017d64347650768764fcb9b689438e8de8df
Donald Jones <jones.donald@gmail.com>
2014-07-06 21:20:31 -0500

    Added bootstrap to the template.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Donald\'s Base Express App' });
+  res.render('index', { title: 'Donald\'s Base Express App', activeNum: 0 });
 });
 
 module.exports = router;
